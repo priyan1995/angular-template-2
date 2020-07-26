c7d3cfdedfe3af4f0a7e65c706d6b8867e345723
priyan <45042030+priyan1995@users.noreply.github.com>
2020-07-26 22:51:24 +0530

    blog single page routing and modules created

--- src/app/blog/blog-routing.module.ts
+++ src/app/blog/blog-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BlogComponent } from './blog.component';
+import { BlogSingleComponent } from './blog-single/blog-single.component';
 
-const routes: Routes = [{ 
-  path: 'view', 
-  component: BlogComponent 
-}];
+const routes: Routes = [
+  {
+    path:'',
+    component:BlogComponent
+  },
+  { 
+    path: 'view', 
+    component: BlogSingleComponent  
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogComponent } from './blog.component';
+import { BlogSingleComponent } from './blog-single/blog-single.component';
 
 
 @NgModule({
-  declarations: [BlogComponent],
+  declarations: [
+    BlogComponent,
+    BlogSingleComponent
+  ],
   imports: [
     CommonModule,
     BlogRoutingModule
