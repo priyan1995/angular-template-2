f77e57ed64a2f4e86822dd2877a7bc142e6aebaf
priyan <45042030+priyan1995@users.noreply.github.com>
2020-08-16 22:39:01 +0530

    contact us page firebase integration

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
+  contact_page;
+  cont:any;
+  map;
+  banner;
+  url;
+  constructor( db: AngularFireDatabase,private hostElement: ElementRef ) { 
 
-  ngOnInit(): void {
+    
+
+    db.list('/contact-page').valueChanges()
+    .subscribe( contact_page =>{
+      
+      this.contact_page = contact_page;
+      var cont = this.contact_page[0];
+      this.banner = cont.banner_img;
+      this.map = cont.map;
+      var mp = this.map;
+
+      const iframe = this.hostElement.nativeElement.querySelector('iframe');
+      this.url = mp;
+      iframe.src = this.url;
+    });
+
+
+  }
+
+  ngOnInit(): void {   
   }
 
 }
