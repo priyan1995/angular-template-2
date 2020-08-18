edddb9a7b20e7956bbb6d3b5fc8a38afb2f75f75
priyan <45042030+priyan1995@users.noreply.github.com>
2020-08-19 00:15:13 +0530

    ts service called for services page

--- src/app/our-service/our-service.component.ts
+++ src/app/our-service/our-service.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-our-service',
   templateUrl: './our-service.component.html',
   styleUrls: ['./our-service.component.css']
 })
+
+
+
 export class OurServiceComponent implements OnInit {
 
-  constructor() { }
+  our_services;
+  
+  constructor( db:AngularFireDatabase ) {
+
+    db.list('/service').valueChanges()
+    .subscribe(our_services => {
+      this.our_services = our_services;
+    });
+
+  }
 
   ngOnInit(): void {
   }
