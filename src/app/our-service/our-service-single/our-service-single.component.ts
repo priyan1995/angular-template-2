167f25e657e881f51a4e6331b29db6ca40924bcc
priyan <45042030+priyan1995@users.noreply.github.com>
2020-08-28 00:14:35 +0530

    services inner page backend fetch

--- src/app/our-service/our-service-single/our-service-single.component.ts
+++ src/app/our-service/our-service-single/our-service-single.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
   selector: 'app-our-service-single',
@@ -7,11 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OurServiceSingleComponent implements OnInit {
 
-  constructor() { }
+  servId: number;
+  o_service_inner;
+  banner
+
+  constructor(
+    private route: ActivatedRoute,
+    db: AngularFireDatabase
+  ) {
+
+    this.route.paramMap.subscribe(params => {
+
+      const servId = +params.get('id');
+      if (servId) {
+        this.servId = servId - 1;
+
+        db.list('/service/service_items/').valueChanges()
+          .subscribe(o_service_inner => {
+            this.o_service_inner = o_service_inner[this.servId];
+          });
+
+
+      }
+    });
+  }
 
   ngOnInit(): void {
-
-    
   }
 
 }
--- src/app/our-service/our-service.component.ts
+++ src/app/our-service/our-service.component.ts
@@ -36,6 +36,7 @@ export class OurServiceComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    
   }
 
 }
